8d0b1258a52802d22e3ff667267abe120e6644da
Dario <yotoo2920@yahoo.com>
2017-04-10 03:33:32 -0400

    refactored readPFD

--- server/pdfreader.js
+++ server/pdfreader.js
@@ -1,42 +1,18 @@
-/**
- * Created by Ale on 4/8/2017.
- */
+const pdfboxCliWrap = require('pdfbox-cli-wrap')
 
 //Class format
 module.exports = class PDFreader{
 
-    constructor(arg1, arg2){
-        this.field1 = arg1;
-        this.field2 = arg2;
-    }
-
-    readFile(path){
-        return jsonPDF;
+    readFile(readable, callback) {
+        pdfboxCliWrap.getFormFields(readable)
+            .then(fields=>{
+                callback(fields);
+            })
+            .catch(e=>console.error(e));
     }
 };
 
-//Use it
-var PDFreader = require("./pdfreader");
-let pdf = new PDFreader();
 
 
 
 
-
-//Singleton Module
-var pdfreader = require("moduleName");
-// var ....
-
-//Public
-exports.funct = function(){
-
-};
-
-//Private
-function funct2(){
-
-}
-
-//Use it
-var myModule = require("./pdfreader");
-myModule.funct();
--- server/server.js
+++ server/server.js
@@ -1,27 +1,35 @@
-/**
- * Created by Ale on 3/30/2017.
- */
+var pdfFillForm = require('pdfbox-cli-wrap');
 
+const path = require('path');
+const readablePdf = path.join(__dirname,'resources/form.pdf')
 
-var pdfFillForm = require('pdf-fill-form');
+var PDFreader = require("./pdfreader");
+let pdf = new PDFreader();
 
-pdfFillForm.read('test.pdf')
-    .then(function(result) {
-        console.log(result);
-    }, function(err) {
-        console.log(err);
-    });
+var jsonPDFObj = pdf.readFile(readablePdf, function(fields){
+    //do something with fields
+    console.log(fields);
+});
 
-var fs = require('fs');
+console.log(jsonPDFObj);
 
-pdfFillForm.write('test.pdf', { "myField": "myField fill value" }, { "save": "pdf" } )
-    .then(function(result) {
-        fs.writeFile("test123.pdf", result, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("The file was saved!");
-        });
-    }, function(err) {
-        console.log(err);
-    });
+// pdfFillForm.read(readablePdf)
+//     .then(function(result) {
+//         pdf.readFile(readablePdf)
+//     }, function(err) {
+//         console.log(err);
+//     });
+//
+// var fs = require('fs');
+//
+// pdfFillForm.write('test.pdf', { "myField": "myField fill value" }, { "save": "pdf" } )
+//     .then(function(result) {
+//         fs.writeFile("test123.pdf", result, function(err) {
+//             if(err) {
+//                 return console.log(err);
+//             }
+//             console.log("The file was saved!");
+//         });
+//     }, function(err) {
+//         console.log(err);
+//     });
