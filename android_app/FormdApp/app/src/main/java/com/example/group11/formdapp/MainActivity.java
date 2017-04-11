7270fcba21ed08a3abdfbfabfa4a852ced483920
Jorge Varela <Jorge Varela>
2017-04-11 18:14:27 -0400

    mmm it should work this time

---
+++ android_app/FormdApp/app/src/main/java/com/example/group11/formdapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.group11.formdapp;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyMainActivity";
+
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mBluetoothLeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate called");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "The permission to get BLE location data is required", Toast.LENGTH_SHORT).show();
+            }else{
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else{
+            Toast.makeText(this, "Location permissions already granted", Toast.LENGTH_SHORT).show();
+        }
+
+        mBluetoothLeScanner.startScan(mScanCallback);
+    }
+
+
+    protected ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            ScanRecord mScanRecord = result.getScanRecord();
+            byte[] manufacturerData = mScanRecord.getManufacturerSpecificData(224);
+            int mRssi = result.getRssi();
+
+            Log.i(TAG, "toString: " +  result.toString());
+            Log.i(TAG, "Uuids: " + result.getDevice().getUuids());
+            Log.i(TAG, "Name: " + result.getDevice().getName());
+            Log.i(TAG, "Device: " + result.getDevice().toString());
+            Log.i(TAG, "ScanRecord bytes: " + Arrays.toString(result.getScanRecord().getBytes()));
+            Log.i(TAG, "mRssi: " + mRssi + "\n\n\n");
+        }
+    };
+}
