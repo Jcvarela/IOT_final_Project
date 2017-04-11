b7b4e469552a7dc2f70e0ccb6dc4e66e546e1e17
Jorge <jcvarela@live.com>
2017-04-11 05:26:20 -0400

    GOT BEACON WORKINGgit statusgit statusgit statusgit status

--- android_app/Form_d/app/src/main/java/com/example/Group11/formd/MainActivity.java
+++ android_app/Form_d/app/src/main/java/com/example/Group11/formd/MainActivity.java
@@ -1,145 +1,69 @@
 package com.example.group11.formd;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
-import android.bluetooth.le.ScanSettings;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BluetoothAdapter mBluetoothAdapter; //require for any bluetooth activity (represent the device own bluetooth, there is only one, so play nice)
-    private final static int REQUEST_ENABLE_BT = 1;
+    private static final String TAG = "MyMainActivity";
 
-    private ArrayList<BluetoothDevice> mLeDevices;
-    private boolean mScanning;
-    private Handler mHandler; //send and process messengers
-    private BluetoothLeScanner mLEScanner; //to scan for bluetooth at low energy
-    private ScanSettings settings;
-    private List<ScanFilter> filters;
-    private BluetoothGatt mGatt;
-    private int CompanyIdentifier = 0x0059;
 
-    private TextView IdTextview;
-    private TextView DistTextview;
-    private TextView RSSITextview;
-    private TextView RSSIonemeterTextview;
-    private int ratiodb;
-    private float ratiolinear;
-    private float Distance;
-    private TextView view;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mBluetoothLeScanner;
 
-    private static final long SCAN_PERIOD = 10000;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate called");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        view = (TextView) findViewById(R.id.info);
-        init();
-    }
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
 
-    public void init(){
-
-        //turn off application if bluetooth_le is not supported by the device
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this,"not supported", Toast.LENGTH_SHORT).show();
-            finish();
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "The permission to get BLE location data is required", Toast.LENGTH_SHORT).show();
+            }else{
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else{
+            Toast.makeText(this, "Location permissions already granted", Toast.LENGTH_SHORT).show();
         }
 
-        mHandler = new Handler();
-
-        //Initialized bluetooth adapter
-        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-
-        // Ensures Bluetooth is available on the device and it is enabled. If not,
-        // displays a dialog requesting user permission to enable Bluetooth.
-        if(mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()){
-            //ask user to enable bluetooth
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-        else {
-//            if(Build.VERSION.SDK_INT >= 21){
-//                mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
-//                settings = new ScanSettings.Builder()
-//                        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-//                        .build();
-//                filters = new ArrayList<ScanFilter>();
-//            }
-           scanLeDevices(true);
-        }
-    }
-
-    /**
-     * Activity for scanning and displaying available BLE devices.
-     */
-    private void scanLeDevices(final boolean enable){
-
-        if (enable) {
-            mScanning = true;
-            mBluetoothAdapter.startLeScan(mLeScanCallback);
-
-            // Stops scanning after a pre-defined scan period.
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mScanning = false;
-                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
-                }
-            }, SCAN_PERIOD);
-        } else {
-            mScanning = false;
-            mBluetoothAdapter.stopLeScan(mLeScanCallback);
-        }
+        mBluetoothLeScanner.startScan(mScanCallback);
     }
 
 
-    // Device scan callback.
-    private BluetoothAdapter.LeScanCallback mLeScanCallback =
-            new BluetoothAdapter.LeScanCallback() {
+    protected ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            ScanRecord mScanRecord = result.getScanRecord();
+            byte[] manufacturerData = mScanRecord.getManufacturerSpecificData(224);
+            int mRssi = result.getRssi();
 
-                @Override
-                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-                    view.setText("found something");
-
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                        }
-                    });
-
-                }
-            };
-
+            Log.i(TAG, "toString: " +  result.toString());
+            Log.i(TAG, "Uuids: " + result.getDevice().getUuids());
+            Log.i(TAG, "Name: " + result.getDevice().getName());
+            Log.i(TAG, "Device: " + result.getDevice().toString());
+            Log.i(TAG, "ScanRecord bytes: " + Arrays.toString(result.getScanRecord().getBytes()));
+            Log.i(TAG, "mRssi: " + mRssi + "\n\n\n");
+        }
+    };
 }
--- android_app/Form_d_3rdParty/app/src/main/java/com/example/group11/formd/MainActivity.java
+++ android_app/Form_d_3rdParty/app/src/main/java/com/example/group11/formd/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.group11.formd;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.RemoteException;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -19,11 +34,20 @@ import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer{
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1 ;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mBluetoothLeScanner;
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "MyMainActivity";
+    private static final String TAG2 = "FUCK_THIS_SHIT";
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
     private Button startButton;
     private Button stopButton;
@@ -59,6 +83,8 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
     }
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "onCreate called");
@@ -71,71 +97,159 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
         startButton.setOnClickListener((v)->{startBeaconMonitoring();});
         stopButton.setOnClickListener((v)->{stopBeaconMonitoring();});
 
-
-        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1234);
+       // requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1234);
 
         beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(IBEACON_LAYOUT));
+        //beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(IBEACON_LAYOUT));
         beaconManager.bind(this);
+
+        //other shit
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+        setScanSettings();
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "The permission to get BLE location data is required", Toast.LENGTH_SHORT).show();
+            }else{
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else{
+            Toast.makeText(this, "Location permissions already granted", Toast.LENGTH_SHORT).show();
+        }
+
+        mBluetoothLeScanner.startScan(mScanCallback);
+//        mBluetoothLeScanner.startScan(null, mScanSettings, mScanCallback);
+        Log.i(TAG2, "Start: " + isLocationEnabled(this.getApplicationContext()) + " permission " + permissionCheck);
     }
 
+    //More Shit ===================================================================================
+    protected ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            ScanRecord mScanRecord = result.getScanRecord();
+            byte[] manufacturerData = mScanRecord.getManufacturerSpecificData(224);
+            int mRssi = result.getRssi();
+
+            Log.i(TAG2, "toString: " +  result.toString());
+            Log.i(TAG2, "Uuids: " + result.getDevice().getUuids());
+            Log.i(TAG2, "Name: " + result.getDevice().getName());
+            Log.i(TAG2, "Device: " + result.getDevice().toString());
+            Log.i(TAG2, "ScanRecord by: " + Arrays.toString(result.getScanRecord().getBytes()));
+            Log.i(TAG2, "mRssi: " + mRssi + "\n\n\n");
+        }
+    };
+
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        }else{
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+
+
+    }
+
+
+    private ScanSettings mScanSettings;
+
+    private void setScanSettings() {
+        ScanSettings.Builder mBuilder = new ScanSettings.Builder();
+        mBuilder.setReportDelay(0);
+        mBuilder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER);
+        mScanSettings = mBuilder.build();
+    }
+    //============================================================================================
 
 
     @Override
     public void onBeaconServiceConnect() {
         Log.i(TAG, "onBeaconServicesConnect called");
 
-        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+        beaconManager.addMonitorNotifier(new MonitorNotifier() {
             @Override
             public void didEnterRegion(Region region) {
-                if(!entryMessageRaised){
+                Log.i(TAG, "I just saw a beacon for the first time!");
+               /* if(!entryMessageRaised){
                     showAlert("didExitRegion", "Exiting region " + region.getUniqueId() +
                     " Beacon detected UUID/major/minor: " + region.getId1() +
                     "/" + region.getId2() + "/" + region.getId3());
 
                     entryMessageRaised = true;
-                }
+                }*/
 
             }
 
             @Override
             public void didExitRegion(Region region) {
-                if(!entryMessageRaised){
+                Log.i(TAG, "I no longer see a beacon");
+               /* if(!entryMessageRaised){
                     showAlert("didExitRegion", "Exiting region " + region.getUniqueId() +
                             " Beacon detected UUID/major/minor: " + region.getId1() +
                             "/" + region.getId2() + "/" + region.getId3());
 
                     entryMessageRaised = true;
-                }
+                }*/
             }
 
             @Override
             public void didDetermineStateForRegion(int i, Region region) {
+                Log.i(TAG, "I have just switched from seeing/not seeing beacons: "+ i);
                 /*Not implemented*/
             }
         });
 
-        beaconManager.setRangeNotifier(new RangeNotifier() {
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+
+        beaconManager.addRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                if(!rangingMessageRaised && beacons != null && !beacons.isEmpty()){
-                    for(Beacon beacon: beacons){
-                        showAlert("didExitRegion", "Exiting region " + region.getUniqueId() +
-                                " Beacon detected UUID/major/minor: " + region.getId1() +
-                                "/" + region.getId2() + "/" + region.getId3());
-                    }
-                    rangingMessageRaised = true;
+                Log.i(TAG, "inside range");
+
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
                 }
+
+//                if(!rangingMessageRaised && beacons != null && !beacons.isEmpty()){
+//                    for(Beacon beacon: beacons){
+//                        Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+//                        showAlert("didExitRegion", "Exiting region " + region.getUniqueId() +
+//                                " Beacon detected UUID/major/minor: " + region.getId1() +
+//                                "/" + region.getId2() + "/" + region.getId3());
+//                    }
+//                    rangingMessageRaised = true;
+//                }
             }
         });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
     }
 
 
     private void startBeaconMonitoring(){
         Log.i(TAG,"start button press");
+
         try{
-            beaconRegion = new Region("MyBeacons", Identifier.parse("00000000000000000000000000000000"),
-                    Identifier.parse("4"), Identifier.parse("200"));
+            beaconRegion = new Region("MyBeacons", Identifier.parse("00000000-0000-0000-0000-000000000009"),
+                    Identifier.parse("ffff"), Identifier.parse("2445"));
 
             beaconManager.startMonitoringBeaconsInRegion(beaconRegion);
             beaconManager.startRangingBeaconsInRegion(beaconRegion);
