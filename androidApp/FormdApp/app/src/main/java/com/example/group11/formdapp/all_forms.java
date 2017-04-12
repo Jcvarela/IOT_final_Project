1e0f90f7a9250915d34fdd0a40ae3211a00b5c9d
Jorge <jcvarela@live.com>
2017-04-12 01:26:41 -0400

    fix something

--- androidApp/FormdApp/app/src/main/java/com/example/group11/formdapp/all_forms.java
+++ androidApp/FormdApp/app/src/main/java/com/example/group11/formdapp/all_forms.java
@@ -3,6 +3,7 @@ package com.example.group11.formdapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -89,7 +90,7 @@ public class all_forms extends AppCompatActivity
         } else if (id == R.id.nav_allforms) {
             formListFrag listFrag = new formListFrag();
             FragmentManager manager = getSupportFragmentManager();
-            manager.beginTransaction().replace(R.id.fo, listFrag);
+            manager.beginTransaction().replace(R.id.forFragList, listFrag);
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
--- androidApp/FormdApp/app/src/main/java/com/example/group11/formdapp/formListFrag.java
+++ androidApp/FormdApp/app/src/main/java/com/example/group11/formdapp/formListFrag.java
@@ -2,7 +2,7 @@ package com.example.group11.formdapp;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
