d62755d764c7fb93f70fe621c8e07ca872244dbc
Dario <yotoo2920@yahoo.com>
2017-04-12 01:39:06 -0400

    fragment test filling out form added

---
+++ androidApp/FormdApp/app/src/main/java/com/example/group11/formdapp/TestFillOutFrag.java
@@ -0,0 +1,29 @@
+package com.example.group11.formdapp;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TestFillOutFrag extends Fragment {
+
+
+    public TestFillOutFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test_fill_out, container, false);
+    }
+
+}
